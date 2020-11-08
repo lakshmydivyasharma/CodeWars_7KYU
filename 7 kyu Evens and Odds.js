2f8a5dde7e9c6cd4480a854036c24e912562abf0
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-11-08 13:43:50 -0500

    daily code challenge

---
+++ 7 kyu Evens and Odds.js
@@ -0,0 +1,23 @@
+https://www.codewars.com/kata/583ade15666df5a64e000058/train/javascript
+
+7 kyu
+Evens and Odds
+12489% of 201582 of 831ocirion
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+FUNDAMENTALS
+
+function evensAndOdds(num){
+	if(num % 2){
+		return 'd'
+	}
+	return '10'
+}
